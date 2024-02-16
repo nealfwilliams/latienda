6ef4345ec4f66bcc01f250ae89219090c984e5aa
Gavin <gavinramos@gmail.com>
2024-02-15 20:26:54 -0600

    wow variable names... if anyone ever reads this please ignore. cleaned up the code. It makes API keys for admin users now. Therefore safer to use requiring API key for things like marking the order as paid - will make impossible to fake payment unless called from the contract directly.

--- src/utils/authKeys.ts
+++ src/utils/authKeys.ts
@@ -4,22 +4,16 @@ import { NextApiRequest } from 'next'
 require("dotenv").config();
 // Creates a signed hash of key using secret key 
 export const hashKey = (key: string) => {
-  console.log("passed in key ",key, "typeof(key) passed in", typeof(key));
   const secret : string = process.env.SECRET_KEY!
-  console.log("secret= ", secret, "typeof(secret)", typeof(secret))
   const hash = crypto.createHmac('sha256', secret)
-  console.log("hash= ", hash, "typeof(hash)", typeof(hash))
   const updatedHash =  hash.update(key)
-  console.log("updatedHash= ", updatedHash, "typeof(updatedHash)", typeof(updatedHash))
   const  digested = updatedHash.digest('hex')
-  console.log("hash == ", digested)
   return digested
 }
 
 export const createKey = async () => {
   // Randomly generate a key in hex format
   const key : string = crypto.randomBytes(32).toString('hex')
-  console.log("key before hashkey function call= ",key);
   // store hash in database
   const hash = hashKey(key)
   await client.authKey.create({
